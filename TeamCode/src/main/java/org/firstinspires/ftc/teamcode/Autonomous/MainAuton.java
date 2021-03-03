1174e3553cb7700a04409fa212f2b1a1b1382ef9
Jay1532 <bhalalajay722@gmail.com>
2021-03-03 17:14:20 -0600

    Update MainAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MainAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MainAuton.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
 import org.firstinspires.ftc.teamcode.Hardware;
+
+//Parking
