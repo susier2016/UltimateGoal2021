a2fe4b6b5465dae5215055f1272668a0d4cd49cc
susier2016 <susmitarenjith@gmail.com>
2021-03-20 08:51:11 -0500

    Susie - Updated autonomous code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MainAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MainAuton.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.teamcode.Hardware;
 
 @Autonomous(name = "Main Autonomous", group = "Linear OpMode")
@@ -13,6 +15,7 @@ public class MainAuton extends LinearOpMode {
 
     //Define the Motors and Servos here to not rely on referencing the robot variable to access the motors and servos
     DcMotor leftFront, rightFront, leftBack, rightBack, intakeMotor, leftLauncher, rightLauncher, wobbleGoalArm;
+    Servo pushServo;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -30,6 +33,7 @@ public class MainAuton extends LinearOpMode {
         intakeMotor = robot.intakeMotor;
         rightLauncher = robot.rightLauncher;
         leftLauncher = robot.leftLauncher;
+        pushServo = robot.pushServo;
 
         //Motors and servos for wobble goal
         //wobbleGoalArm = robot.wobbleGoalArm;
@@ -40,22 +44,24 @@ public class MainAuton extends LinearOpMode {
         rightFront.setPower(1);
         rightBack.setPower(1);
 
-        sleep(milliseconds: 2500);
+        sleep(2500);
 
         leftFront.setPower(0);
         leftBack.setPower(0);
         rightFront.setPower(0);
         rightBack.setPower(0);
 
-        rightLauncher.setPower(1)
-        leftLauncher.setPower(1)
+        rightLauncher.setPower(1);
+        leftLauncher.setPower(1);
 
+        //Launches 3 disks
         for (int i = 0; i < 3; i++) {
             pushServo.setPosition(Servo.MAX_POSITION);
+            sleep(500);
             pushServo.setPosition(Servo.MIN_POSITION);
         }
 
-        sleep(milliseconds: 10000);
+        sleep(10000);
 
         //Robot moves back and lands on parking line
         leftFront.setPower(1);
@@ -63,7 +69,7 @@ public class MainAuton extends LinearOpMode {
         rightFront.setPower(-1);
         rightBack.setPower(-1);
 
-        sleep(milliseconds: 2500);
+        sleep(2500);
 
         //ADD MOVEMENT TO RIGHT OR LEFT IF NEEDED
     }
